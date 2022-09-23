d59da947235ea43188dc2236afac45aad15ae7f1
Andy Holman <andy.holman@n2co.com>
2022-09-23 09:02:14 -0400

    Latest maps and plugins

--- .config/nvim/lua/zenom/maps.lua
+++ .config/nvim/lua/zenom/maps.lua
@@ -10,3 +10,18 @@ keymap.set('n', '<C-L>', '<C-W><C-L>') -- move
 keymap.set('n', '<C-H>', '<C-W><C-H>') -- move
 
 keymap.set('i', 'jj', '<ESC>')
+
+
+keymap.set('n', '<leader>s', '<CMD>Telescope find_files<CR>')
+keymap.set('n', '<leader>sg', '<CMD>Telescope live_grep<CR>')
+keymap.set('n', '<leader>sG', '<CMD>Telescope grep_string<CR>')
+keymap.set('n', '<leader>ja', '<CMD>Telescope find_files cwd=app/assets/<CR>')
+keymap.set('n', '<leader>jc', '<CMD>Telescope find_files cwd=app/controllers/<CR>')
+keymap.set('n', '<leader>ji', '<CMD>Telescope find_files cwd=app/interactors/<CR>')
+keymap.set('n', '<leader>jm', '<CMD>Telescope find_files cwd=app/models/<CR>')
+keymap.set('n', '<leader>jv', '<CMD>Telescope find_files cwd=app/views/<CR>')
+keymap.set('n', '<leader>jp', '<CMD>Telescope find_files cwd=app/policies/<CR>')
+keymap.set('n', '<leader>jT', '<CMD>Telescope find_files cwd=test/<CR>')
+
+keymap.set('n', '<leader>ft', ':ToggleTerm dir=git_dir direction=float<CR>')
+keymap.set('n', '<leader>tb', ':ToggleTerm size=20 dir=git_dir direction=horizontal<CR>')
--- .config/nvim/lua/zenom/plugins.lua
+++ .config/nvim/lua/zenom/plugins.lua
@@ -2,17 +2,29 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+-- LSP, Treesitter, 
 Plug 'tpope/vim-markdown'
 Plug 'nvim-lualine/lualine.nvim' -- statusline
 Plug 'onsails/lspkind-nvim' -- vscode-like pictograms
 Plug 'MunifTanjim/prettier.nvim'
 Plug 'kyazdani42/nvim-web-devicons' -- File icons
+Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'nvim-telescope/telescope-file-browser.nvim'
 Plug 'windwp/nvim-autopairs'
 Plug 'glepnir/lspsaga.nvim'
 Plug('catppuccin/nvim', {as = 'catppuccin'})
 Plug 'kyazdani42/nvim-tree.lua'
+Plug 'neovim/nvim-lspconfig'
+Plug 'norcalli/nvim-colorizer.lua'
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'akinsho/toggleterm.nvim'
+--Plug 'dinhhuy258/git.nvim'
+
+-- testing
+Plug 'onsails/lspkind-nvim'
+--Plug 'hrsh7th/cmp-buffer'
+--Plug 'hrsh7th/cmp-nvim-lsp'
 
 
 -- Plug('neoclide/coc.nvim', {branch = 'release'})
@@ -52,4 +64,5 @@ require("nvim-tree").setup({
   },
 })
 
-
+require('gitsigns').setup()
+require('toggleterm').setup()
