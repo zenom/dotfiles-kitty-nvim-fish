5d7bc470da83b383b398dfd8fefa42135ff198b0
Andy Holman <andy@conspyre.com>
2022-11-01 17:19:44 -0400

    Update treesitter.

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -9,27 +9,27 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = {
-    "lua",
-    "markdown",
-    "markdown_inline",
-    "bash",
-    "python",
-    "elixir",
-    "ruby",
-    "erlang",
-    "fish",
-    "heex",
-    "json",
-    "html",
-    "javascript",
-    "scss",
-  }, -- put the language you want in this array
-  -- ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = {
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"bash",
+		"python",
+		"elixir",
+		"ruby",
+		"erlang",
+		"fish",
+		"heex",
+		"json",
+		"html",
+		"javascript",
+		"scss",
+	}, -- put the language you want in this array
+	-- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 
-  highlight = {
+	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
 	},
@@ -42,5 +42,4 @@ configs.setup({
 		enable = true,
 		enable_autocmd = false,
 	},
-
 })
