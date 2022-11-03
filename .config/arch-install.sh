cafd250b7baa94d2285875a73e33af3f03eda3e5
Andy Holman <andy@conspyre.com>
2022-11-03 09:15:47 -0400

    Latest arch files.

--- .config/arch-install.sh
+++ .config/arch-install.sh
@@ -4,7 +4,7 @@
 pacman -Syu
 
 # get base packages
-pacman -Sy lazygit git neovim tmux wget ttf-jetbrains-mono ripgrep fish htop prettier stylua aws-cli bat exa
+pacman -Sy lazygit git neovim tmux wget ttf-jetbrains-mono ripgrep fish htop prettier stylua aws-cli bat exa ncdu
 
 # update
 curl -L https://fly.io/install.sh | sh
